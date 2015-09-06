b54bda3acb2575a3771607bf2878a0b0be604972
Youye <sun.yo@husky.neu.edu>
2015-09-06 00:48:25 -0400

    pointer

---
+++ array/remove-element.cpp
@@ -0,0 +1,16 @@
+/*
+Given an array and a value, remove all instances of that value in place and return the new length.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length. 
+*/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int store = 0;
+        for(int i =0;i <nums.size(); i++){
+            if(nums[i]!=val)
+                nums[store++]=nums[i];
+        }
+        return store;
+    }
+};
