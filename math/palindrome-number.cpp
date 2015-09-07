d65845563a505d424cda3b2d36c414348615d310
Youye <sun.yo@husky.neu.edu>
2015-09-06 23:09:12 -0400

    iterate half way

---
+++ math/palindrome-number.cpp
@@ -0,0 +1,17 @@
+/*
+Determine whether an integer is a palindrome. Do this without extra space.
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x%10 == 0 && x!=0 )
+            return false;
+        int res = 0;
+        while(x > res){
+            res = res*10 + x%10;
+            x /= 10;
+        }
+        return ((x==res)||(x == res/10));
+    }
+};
