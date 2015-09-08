f446cf5fe0b52d3e07b4268509f4258891397308
Youye <sun.yo@husky.neu.edu>
2015-09-07 23:08:14 -0400

    nice problem!!!

---
+++ bit-op/single-number-iii.cpp
@@ -0,0 +1,30 @@
+/*
+ Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+For example:
+
+Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+
+Note:
+
+    The order of the result is not important. So in the above example, [5, 3] is also correct.
+    Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+*/
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int diff = 0;
+        for(auto n : nums)
+            diff ^= n;
+        int lastbit = (diff)&(diff-1)^diff;
+        vector<int> res(2,0);
+        for(auto n : nums){
+            if(lastbit&n)
+                res[0]^=n;
+            else
+                res[1]^=n;
+        }
+        return res;
+    }
+};
